389d57ac062135f60d6fe8742872ff923e157767
phenomxx <merlinplaycs@gmail.com>
2021-10-29 01:44:50 +0500

    Update gamecontext.cpp

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -4732,7 +4732,7 @@ void CGameContext::UseItem(int ClientID, int ItemID, int Count, int Type)
 			}
 			else if(ItemID == TOMATE)
 			{
-				PackOne += 60;
+				PackOne += 40;
 				if(i == Count-1)
 				{
 					SendChatTarget_Localization(-1, CHATCATEGORY_DEFAULT, _("{str:name} used {str:used} x{int:num} and get {int:pvars} exp"),
@@ -4743,7 +4743,7 @@ void CGameContext::UseItem(int ClientID, int ItemID, int Count, int Type)
 			}
 			else if(ItemID == POTATO)
 			{
-				PackOne += 80;
+				PackOne += 50;
 				if(i == Count-1)
 				{
 					SendChatTarget_Localization(-1, CHATCATEGORY_DEFAULT, _("{str:name} used {str:used} x{int:num} and get {int:pvars} exp"),
@@ -4754,7 +4754,7 @@ pPlayer->AccData.Exp += PackOne;
 			}
 			else if(ItemID == CARROT)
 			{
-				PackOne += 50;
+				PackOne += 30;
 				if(i == Count-1)
 				{
 					SendChatTarget_Localization(-1, CHATCATEGORY_DEFAULT, _("{str:name} used {str:used} x{int:num} and get {int:pvars} exp"),
